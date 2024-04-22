526ed7d65909b042ef841bce78d654c22a570e2a
enkelan <eno_rami@ymail.com>
2024-04-22 15:56:35 +0200

    added react calendar model

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TodoList from './todolist';
+import CalendarComponent from './CalendarComponent';
 
 export default function App() {
   const [inputValue, setInputValue] = useState('');
@@ -15,21 +17,44 @@ export default function App() {
   };
 
   return (
-    <>
-      <h1>My Todo List</h1>
-      <div className="flexInputButton">
-        <input
-          type="text"
-          placeholder="Type your todo list"
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyPress={handleKeyPress}
-        />
-        <button className="btn" type="sumbit">
-          Add Todo
-        </button>
+    <Router>
+      <div>
+        <nav>
+          <ul className="calendar-nav">
+            <li>
+              <Link to="/">Todo List</Link>
+            </li>
+            <li>
+              <Link to="/calendar">Calendar</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/calendar" element={<CalendarComponent />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>My Todo List</h1>
+                <div className="flexInputButton">
+                  <input
+                    type="text"
+                    placeholder="Type your todo list"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyPress={handleKeyPress}
+                  />
+                  <button className="btn" type="submit">
+                    Add Todo
+                  </button>
+                </div>
+                <TodoList inputValue={inputValue} />
+              </>
+            }
+          />
+        </Routes>
       </div>
-      <TodoList inputValue={inputValue} />
-    </>
+    </Router>
   );
 }
